e4c8871d4b574b49b00f7a24f188310cec94c377
roxyoanes <roxioanes11@gmail.com>
2020-05-28 10:54:00 +0200

    Display list of models

--- src/BrandComponent.js
+++ src/BrandComponent.js
@@ -1,18 +1,23 @@
 import React from "react";
 import { BrowserRouter as Link, useParams } from "react-router-dom";
 
-import { braNameApi } from "./api";
+import { braNameApi, braModelsApi } from "./api";
 
 const BrandComponent = () => {
   const [brandDescription, setBrandDescription] = React.useState([]);
+  const [braModels, setBraModels] = React.useState([]);
 
   let { href } = useParams();
-  console.log("AAAA", href); 
 
   React.useEffect(() => {
     braNameApi(href).then((dataDetails) => {
       setBrandDescription(dataDetails.body);
-      console.log("BBBB", dataDetails.body);
+    });
+  }, [href]);
+
+  React.useEffect(() => {
+    braModelsApi(href).then((dataModels) => {
+      setBraModels(dataModels.collection);
     });
   }, [href]);
 
@@ -24,6 +29,12 @@ const BrandComponent = () => {
       <div className="brand-container">
         <a href={brandDescription.official_url} rel="noreferrer" target="_blank" >{brandDescription.name}</a>
         <img className="brand-img" src={brandDescription.logo} alt="img" />
+        <h4>Bra Models</h4>
+        {braModels.map((obj) => (
+          <div key={obj.name}>
+            <a href={obj.bratabase_url} rel="noreferrer" target="_blank">{obj.name}</a>
+          </div>
+        ))}
       </div>
     </div>
   );
