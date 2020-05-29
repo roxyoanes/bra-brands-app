f40f8a24acd64daea2eec3bedaf6d025d7444313
roxyoanes <roxioanes11@gmail.com>
2020-05-29 15:32:42 +0200

    Display image model

--- src/ModelComponent.js
+++ src/ModelComponent.js
@@ -1,31 +1,38 @@
 import React from "react";
-import { BrowserRouter as Link, useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
-import {modelDetailsApi} from "./api";
+import { modelDetailsApi } from "./api";
 
 const ModelComponent = () => {
-  const [modelDetails, setModelDetails] = React.useState([]);
+  const [modelDetails, setModelDetails] = React.useState(null);
 
-  let { model } = useParams();
+  let { model, href } = useParams();
 
   React.useEffect(() => {
-    modelDetailsApi(model).then((dataDetail) => {
+    modelDetailsApi(href, model).then((dataDetail) => {
       setModelDetails(dataDetail);
-      console.log("UUU", dataDetail);
     });
-  }, [model]);
+  }, [href, model]);
 
-  return(
+  return (
     <div>
-       <Link to="/brands/:href">
+      <Link to="/">
         <button className="btn">Back</button>
       </Link>
       <div>
         <p>Model details</p>
-          {modelDetails ? (<p>{modelDetails.body.name}</p>) : (<p>Loading</p>)}
+        {modelDetails ? (
+          <div>
+            <p>{modelDetails.body.name}</p>
+            <img className="brand-img" src={modelDetails.body.main_picture} alt="model" />
+            <p>{modelDetails.body.type}</p>
+          </div>
+        ) : (
+          <p>Loading</p>
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default ModelComponent;
